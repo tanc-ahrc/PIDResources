1a3303dfd3b1e61dae079ff2f67f1045f9ac226a
FrancesMadden <52208466+FrancesMadden@users.noreply.github.com>
2021-02-22 10:17:48 +0000

    Update list.php

--- build/extensions/list.php
+++ build/extensions/list.php
@@ -1,39 +1,39 @@
 <?php
 
-// Last update 22 Dec 2020
+// Last update 19 Feb 2021
 
 $extensionList["list"] = "extensionCards";
 $blank = array("groups" => array(), "ptitle" => "",
-		"stitle" => "",  "comment" => "", "image" => "", "link" => "");
+    "stitle" => "",  "comment" => "", "image" => "", "link" => "");
 $defaultcard = "list";
 $displaychecked = true;
-		
+$maxcols = false;
+    
 // Still to do an optional table of contents for groups - model on https://github.com/IIIF/awesome-iiif
 
 function buildContents ($groups)
-	{
-	$html = "<h3>Contents</h3><ul>";
+  {
+  $html = "<h3>Contents</h3><ul>";
 
-	foreach ($groups as $gnm => $ga)
-		{$tag = urlencode(strtolower($gnm));
- 		 $html .= "<li>".
-			"<a id=\"$tag-TBC\" class=\"offsetanchor\"></a>".
-			"<a href=\"#$tag\">$gnm</a></li>";}
-	
-	$html .= "</ul>";
+  foreach ($groups as $gnm => $ga)
+    {$tag = urlencode(strtolower($gnm));
+      $html .= "<li>".
+      "<a id=\"$tag-TBC\" class=\"offsetanchor\"></a>".
+      "<a href=\"#$tag\">$gnm</a></li>";}
+  
+  $html .= "</ul>";
 
-	return ($html);
-	}
+  return ($html);
+  }
 
 function extensionCards ($d, $pd)
   {
-	global $blank, $defaultcard, $displaychecked ;
+  global $blank, $defaultcard, $displaychecked, $maxcols;
   $gcontent = "";
-		
-	if (isset($d["file"]) and file_exists($d["file"]))
-		{
-    //$d["file"] = "./awsomelist_groupsfull.json";
-		$dets = getRemoteJsonDetails($d["file"], false, true);
+    
+  if (isset($d["file"]) and file_exists($d["file"]))
+    {
+    $dets = getRemoteJsonDetails($d["file"], false, true);
 
     if (isset($dets["defaultcard"]))
       {$defaultcard = $dets["defaultcard"];}
@@ -54,83 +54,70 @@ function extensionCards ($d, $pd)
         if ($ja) {$dets["list"][] = $ja;}}
       }
 
-    //prg(1, $dets);
-		
-		foreach ($dets["list"] as $lno => $la)
-			{
-			//ensure each of the currently required fields are present.
-			$la = array_merge($blank, $la);
-			
-			foreach ($la["groups"] as $k => $gnm)
-					{            
-					/*$c = false;
-					if (preg_match("/^Digital.+$/", $gnm, $m))
-						{$c = true;
-							echo "CCCCCCCCCCCCCCCCC\n"; prg(0, $gnm);}
-					else
-						{echo "@@@".$gnm."@@@\n";}*/
-										
-					if (!isset($dets["groups"] [$gnm] ))
-						{$dets["groups"] [$gnm]  = array(
-							"comment" => "",
-							"card" => $defaultcard,
-							"config" => array(),
-							);}
-														
-					if (!isset($dets["groups"] [$gnm] ["card"]))
-						{$dets["groups"] [$gnm] ["card"] = "list";}
+    
+    foreach ($dets["list"] as $lno => $la)
+      {
+      //ensure each of the currently required fields are present.
+      $la = array_merge($blank, $la);
+      
+      $dga = array("comment" => "", "card" => $defaultcard, 
+        "config" => array(), "maxcols" => 3);
+      
+      foreach ($la["groups"] as $k => $gnm)
+          {                                
+          if (!isset($dets["groups"][$gnm] ))
+            {$dets["groups"][$gnm]  = $dga;}
+          else
+						{$dets["groups"][$gnm] = array_merge($dga, $dets["groups"][$gnm]);}
+                            
+          if (!isset($dets["groups"][$gnm]["card"]))
+            {$dets["groups"][$gnm]["card"] = "list";}
 
-					if (!function_exists("build".ucfirst($dets["groups"] [$gnm] ["card"])."Card"))
-						{$cfn = "buildSimpleCard";}
-					else
-						{$cfn = "build".ucfirst($dets["groups"] [$gnm] ["card"])."Card";}
-					
-					if (!isset($dets["groups"] [$gnm] ["html"]))
-						{//echo "########$gnm###############\n";
-							$dets["groups"] [$gnm] ["html"] = startGroupHtml (
-							$gnm, $dets["groups"] [$gnm] ["comment"],
-							$dets["groups"] [$gnm] ["card"], $dets["tableofcontents"]);
-
-							//if ($c) {prg(0, $dets["groups"] [$gnm] );}
-			
-						}
-						
-					$dets["groups"] [$gnm] ["html"] .= call_user_func_array($cfn, array($la));					
-					}
-			}
+          if (!function_exists("build".ucfirst($dets["groups"][$gnm]["card"])."Card"))
+            {$cfn = "buildSimpleCard";}
+          else
+            {$cfn = "build".ucfirst($dets["groups"][$gnm]["card"])."Card";}
+          
+          if (!isset($dets["groups"][$gnm]["html"]))
+            {$dets["groups"][$gnm]["html"] = startGroupHtml (
+             $gnm, $dets["groups"][$gnm]["comment"],
+             $dets["groups"][$gnm]["card"], $dets["tableofcontents"]);}
+          
+          $maxcols = $dets["groups"][$gnm]["maxcols"];
+          $dets["groups"][$gnm]["html"] .= call_user_func_array($cfn, array($la));          
+          }
+      }
 
     // organise children
-		foreach ($dets["groups"] as $gnm => $ga)
-				{
+    foreach ($dets["groups"] as $gnm => $ga)
+        {
         $pc = explode ("|", $gnm);
 
         if (count($pc) > 1)
           {
-          if (in_array($dets["groups"] [$gnm] ["card"], array("list")))
-            {$dets["groups"] [trim($pc[0])] ["html"] .=
-                "</ul><br/>".$dets["groups"] [$gnm] ["html"] ;}
+          if (in_array($dets["groups"][$gnm]["card"], array("list")))
+            {$dets["groups"][trim($pc[0])]["html"] .=
+                "</ul><br/>".$dets["groups"][$gnm]["html"] ;}
           else
-            {$dets["groups"] [trim($pc[0])] ["html"] .=
-                "</div><br/>".$dets["groups"] [$gnm] ["html"] ;}
+            {$dets["groups"][trim($pc[0])]["html"] .=
+                "</div><br/>".$dets["groups"][$gnm]["html"] ;}
 
-          unset($dets["groups"] [$gnm]);
+          unset($dets["groups"][$gnm]);
           }
         }
         
-		foreach ($dets["groups"] as $gnm => $ga)
-				{
-				if (!isset($ga["html"]))
-					{/*prg(0, $gnm);
-					 prg(0, $dets["groups"][$gnm]);*/}
-				 if (in_array($dets["groups"] [$gnm] ["card"], array("list")))
-					{$gcontent .= $ga ["html"]."</ul><br/>";}
-				else
-					{$gcontent .= $ga ["html"]."</div><br/>";}
-				}
-			
-		$pd["extra_css"] .= "
+    foreach ($dets["groups"] as $gnm => $ga)
+        {
+        if (!isset($ga["html"])) {/*ERROR*/}
+        if (in_array($dets["groups"][$gnm]["card"], array("list")))
+          {$gcontent .= $ga ["html"]."</ul><br/>";}
+        else
+          {$gcontent .= $ga ["html"]."</div><br/>";}
+        }
+      
+    $pd["extra_css"] .= "
 .card-img {
-	width: auto;
+  width: auto;
   max-width: 100%;
   max-height:200px;
   display: block;
@@ -138,9 +125,8 @@ function extensionCards ($d, $pd)
   margin-right: auto;
   padding: 10px;
 }
-
 .card-img-top {
-	width: auto;
+  width: auto;
   max-width: 100%;    
   max-height:128px;
   display: block;
@@ -148,38 +134,34 @@ function extensionCards ($d, $pd)
   margin-right: auto;
   padding: 10px;
 }
-
 .nodec:link, .nodec:visited, .nodec:hover, .nodec:active {
-	text-decoration: none;
-	color: inherit;
-	}
-
+  text-decoration: none;
+  color: inherit;
+  }
 .card-hov:hover {
-	opacity: 0.7;
+  opacity: 0.7;
 }
-
 .offsetanchor {
-	position: relative;
+  position: relative;
   top: -75px;
 }
-
 ";
 
-		// Check if a table of contents should be added.
-		if (!isset($dets["tableofcontents"])) {$dets["tableofcontents"] = false;}
-		if ($dets["tableofcontents"])
-			{$tb = buildContents ($dets["groups"]);}
-		else
-			{$tb = "";}
-			
+    // Check if a table of contents should be added.
+    if (!isset($dets["tableofcontents"])) {$dets["tableofcontents"] = false;}
+    if ($dets["tableofcontents"])
+      {$tb = buildContents ($dets["groups"]);}
+    else
+      {$tb = "";}
+      
     $d["content"] = positionExtraContent ($d["content"], $tb.$gcontent);
-		}
+    }
 
   return (array("d" => $d, "pd" => $pd));
   }
 
 function buildFullCard ($la)
-  {	 
+  {   
   if ($la["link"])
     {$ltop= "<a href=\"$la[link]\" class=\"stretched-link nodec\">";
       $lbottom = "</a>";
@@ -188,10 +170,9 @@ function buildFullCard ($la)
     {$ltop= "";
      $lbottom = "";
      $hclass =  "";}
-				
-  ob_start();			
+        
+  ob_start();      
   echo <<<END
-
 <div class="card mb-3 $hclass" style="width: 100%;">
   $ltop<div class="row no-gutters">
     <div class="col-md-4  my-auto" >
@@ -206,30 +187,43 @@ function buildFullCard ($la)
     </div>
   </div>$lbottom
 </div>
-
 END;
-		$html = ob_get_contents();
-		ob_end_clean(); // Don't send output to client
+    $html = ob_get_contents();
+    ob_end_clean(); // Don't send output to client
 
-		return ($html);
-		}
+    return ($html);
+    }
 
- function buildSimpleCard ($la) {			
-		if ($la["link"])
-				{$ltop= "<a href=\"$la[link]\" class=\"stretched-link nodec\">";
-					$lbottom = "</a>"	;
-      $hclass =  "card-hov";}
-		else
-				{$ltop= "";
-					$lbottom = "";
-      $hclass =  "";}
-				
-		ob_start();			
-		echo <<<END
-		
-  <div class="col mb-4 $hclass";>
+function BS_ColClass ($max=3)
+  {
+  $cno = intval($max);
+  if ($cno > 12) {$cno = 12;}
+  if ($cno < 1) {$cno = 1;}
+  $cno = intval(12/$cno);
+  $class = "col-".$cno;
+  return ($class);
+  }
+
+function buildSimpleCard ($la) { 
+	global $maxcols;
+	     
+  if ($la["link"])
+    {$ltop= "<a href=\"$la[link]\" class=\"stretched-link nodec\">";
+     $lbottom = "</a>";
+     $hclass =  "card-hov";}
+  else
+    {$ltop= "";
+     $lbottom = "";
+     $hclass =  "";}
+  
+  $cc = BS_ColClass ($maxcols);
+  
+  ob_start();      
+  echo <<<END
+      
+  <div class="$cc mb-4 $hclass";>
     <div class="card" title="$la[ptitle]">
-			$ltop
+      $ltop
       <img class="card-img-top" src="$la[image]" alt="$la[ptitle]">
       $lbottom
       <div class="card-body">
@@ -238,54 +232,56 @@ END;
       </div>
     </div>
   </div>
-
 END;
-		$html = ob_get_contents();
-		ob_end_clean(); // Don't send output to client
+    $html = ob_get_contents();
+    ob_end_clean(); // Don't send output to client
 
-		return ($html);
-		}
-		
- function buildImageCard ($la) {			
-		if ($la["link"])
-				{$ltop= "<a href=\"$la[link]\" class=\"stretched-link nodec\">";
-					$lbottom = "</a>"	;
-      $hclass =  "card-hov";}
-		else
-				{$ltop= "";
-					$lbottom = "";
-      $hclass =  "";}
-				
-		ob_start();			
-		echo <<<END
-		
-  <div class="col mb-4 $hclass">
+    return ($html);
+    }
+    
+function buildImageCard ($la) { 
+	global $maxcols;
+	     
+  if ($la["link"])
+    {$ltop= "<a href=\"$la[link]\" class=\"stretched-link nodec\">";
+     $lbottom = "</a>"  ;
+     $hclass =  "card-hov";}
+  else
+    {$ltop= "";
+     $lbottom = "";
+     $hclass =  "";}
+  
+  $cc = BS_ColClass ($maxcols);        
+  
+  ob_start();      
+  echo <<<END
+    
+  <div class="$cc mb-4 $hclass">
     <div class="card" title="$la[ptitle]">
-			$ltop
+      $ltop
       <img class="card-img-top" src="$la[image]" alt="$la[ptitle]">
       $lbottom
     </div>
   </div>
-
 END;
-		$html = ob_get_contents();
-		ob_end_clean(); // Don't send output to client
+    $html = ob_get_contents();
+    ob_end_clean(); // Don't send output to client
 
-		return ($html);
-		}
+    return ($html);
+    }
 
  function buildListCard ($la) {
-   	global $displaychecked ;
+     global $displaychecked ;
     
-	 	if ($la["link"])
-				{$ltop= "<a href=\"$la[link]\" class=\"\">";
-					$lbottom = "</a>"	;}
-		else
-				{$ltop= "";
-					$lbottom = "";}
+     if ($la["link"])
+        {$ltop= "<a href=\"$la[link]\" class=\"\">";
+          $lbottom = "</a>"  ;}
+    else
+        {$ltop= "";
+          $lbottom = "";}
 
-		if ($la["comment"])
-			{$la["comment"] = " - ".$la["comment"];}
+    if ($la["comment"])
+      {$la["comment"] = " - ".$la["comment"];}
 
     if ($displaychecked )
       {
@@ -296,20 +292,20 @@ END;
       }
     else
       {$checked = "";}
-				
-		ob_start();			
-		echo <<<END
+        
+    ob_start();      
+    echo <<<END
 <li>$ltop$la[ptitle]$lbottom$la[comment]$checked</li>
 END;
-		$html = ob_get_contents();
-		ob_end_clean(); // Don't send output to client
+    $html = ob_get_contents();
+    ob_end_clean(); // Don't send output to client
 
-		return ($html);
-		}
+    return ($html);
+    }
 
 function startGroupHtml ($gnm, $comment, $card, $tbc)
-	{
-	$html = "";
+  {
+  $html = "";
   $tag = urlencode(strtolower($gnm));
   $hno = 3;
   
@@ -320,28 +316,30 @@ function startGroupHtml ($gnm, $comment, $card, $tbc)
      $gnm = $pc[1];
      $hno = 4;}  
 
-	if ($tbc)
-		{$anchor = "<a id=\"$tag\" class=\"anchor offsetanchor\" ".
-				"aria-hidden=\"true\" href=\"#${tag}-TBC\"></a>";
-		 $alink = "<a class=\"anchor nodec\" aria-hidden=\"true\" ".
-				"href=\"#${tag}-TBC\">$gnm</a>";}
-	else
-		{$anchor = "";
-		 $alink = "$gnm";}
+  if ($tbc)
+    {$anchor = "<a id=\"$tag\" class=\"anchor offsetanchor\" ".
+        "aria-hidden=\"true\" href=\"#${tag}-TBC\"></a>";
+     $alink = "<a class=\"anchor nodec\" aria-hidden=\"true\" ".
+        "href=\"#${tag}-TBC\">$gnm</a>";}
+  else
+    {$anchor = "";
+     $alink = "$gnm";}
 
-	$gtop = "<h${hno}>$anchor$alink</h${hno}><p>".$comment."</p>";
+  $gtop = "<h${hno}>$anchor$alink</h${hno}><p>".$comment."</p>";
 
-	if (in_array($card, array("image")))
-		{$html  = "$gtop<div class=\"row row-cols-1 ".
-			"row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5\">";}						
-	else if (in_array($card, array("list")))
-		{$html = "$gtop<ul>";}							
-	else if (in_array($card, array("full")))
-		{$html = "$gtop<div class=\"card-column\">";}
-	else //if (in_array($card, array("simple"))) or anything else
-		{$html = "$gtop<div class=\"card-deck\">";}
+	// this was removed to allow the maxcol value to control the number of
+	// max cols for the image cards as well as the simple cards.
+  /*if (in_array($card, array("image")))
+    {$html  = "$gtop<div class=\"row row-cols-1 ".
+      "row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5\">";}            
+  else*/ if (in_array($card, array("list")))
+    {$html = "$gtop<ul>";}              
+  else if (in_array($card, array("full")))
+    {$html = "$gtop<div class=\"card-column\">";}
+  else //if (in_array($card, array("simple"))) or anything else
+    {$html = "$gtop<div class=\"card-deck\">";}
 
-	return ($html);
-	}
+  return ($html);
+  }
     
 ?>
